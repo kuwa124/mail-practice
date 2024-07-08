acedb1548bc8756091c4d438345656c37f619254
kuwa124 <c0mwgave1407@gmail.com>
2024-07-08 16:33:49 +0900

    🎨 画面構成の雛型作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-import Header from ' <prefix>/components/Header/Header';
-import Sidebar from ' <prefix>/components/Sidebar/Sidebar';
-import Image from 'next/image';
+import Toolbar from ' <prefix>/components/Toolbar';
+import Header from ' <prefix>/components/Header';
+import Sidebar from ' <prefix>/components/Sidebar';
+import MailBox from ' <prefix>/components/MailBox';
+import MailView from ' <prefix>/components/MailView';
 
 export default function Home() {
   return (
     <div className='flex flex-col h-screen bg-gray-100'>
       <Header />
-
+      <Toolbar />
       {/* メインコンテンツ */}
       <div className='flex flex-1 overflow-hidden'>
         {/* サイドバー */}
         <Sidebar />
+        <MailBox />
+        <MailView />
       </div>
     </div>
   );
---
+++ src/components/MailBox/index.tsx
@@ -0,0 +1,7 @@
+export default function MailBox() {
+  return (
+    <div className='w-1/4 bg-blue-700 text-white p-4'>
+      <h2>メールボックス</h2>
+    </div>
+  );
+}
