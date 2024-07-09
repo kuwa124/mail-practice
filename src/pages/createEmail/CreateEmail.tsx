a61b5b91094670c2091c42bc07b5622a74b13638
kuwa124 <c0mwgave1407@gmail.com>
2024-07-09 16:03:04 +0900

    🚧 エラーになるので、一時的にコメント化

--- src/pages/createEmail/CreateEmail.tsx
+++ src/pages/createEmail/CreateEmail.tsx
@@ -1,17 +1,13 @@
-// src/pages/createEmail/CreateEmail.tsx
-import Header from '@/components/Header';
-import NewMessage from '@/components/NewMessage';
+// // src/pages/createEmail/CreateEmail.tsx
+// import Header from '@/components/Header';
+// import NewMessage from '@/components/NewMessage';
+// import { useToolbar } from '@/components/Toolbar/useToolbar';
 
-export default function Home() {
-  const handleClose = () => {
-    console.log('Close button clicked');
-    // ここで必要な閉じる操作を実装します
-  };
-
-  return (
-    <div className='flex flex-col h-screen bg-gray-100'>
-      <Header />
-      <NewMessage /> {/* onCloseプロパティを渡す */}
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div className='flex flex-col h-screen bg-gray-100'>
+//       <Header />
+//       <NewMessage onClose={() => {handleSubmit}} />
+//     </div>
+//   );
+// }
