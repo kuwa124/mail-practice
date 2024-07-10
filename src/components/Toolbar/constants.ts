679f26b76ecefcd6f9fa35f70e047e89a82e4bc8
kuwa124 <c0mwgave1407@gmail.com>
2024-07-10 12:39:39 +0900

    💡 コメントの追加

--- src/components/Toolbar/constants.ts
+++ src/components/Toolbar/constants.ts
@@ -6,34 +6,50 @@ import {
   faFlag,
   faTrash,
   faEllipsisH,
-  faSearch,
-  faChevronDown,
-  faTimes,
 } from '@fortawesome/free-solid-svg-icons';
 
 // ツールバーのボタン情報を定義
 export const TOOLBAR_BUTTONS = [
   {
     icon: faEnvelope,
-    message: '新しく届いたメッセージを確認',
-    ariaLabel: 'メッセージ確認',
+    message: '新しく届いたメッセージを確認', // アイコンの説明
+    ariaLabel: 'メッセージ確認', // アクセシビリティラベル
   },
   {
     icon: faPen,
-    message: '新しいメッセージを作成',
-    ariaLabel: '新規メッセージ',
-    action: 'newMessage',
+    message: '新しいメッセージを作成', // アイコンの説明
+    ariaLabel: '新規メッセージ', // アクセシビリティラベル
+    action: 'newMessage', // 実行されるアクション
   },
-  { icon: faReply, message: '返信', ariaLabel: '返信' },
-  { icon: faFlag, message: 'マークをつける', ariaLabel: 'マーク' },
-  { icon: faTrash, message: 'ゴミ箱に移動', ariaLabel: 'ゴミ箱' },
-  { icon: faEllipsisH, message: 'その他の操作', ariaLabel: 'その他' },
+  {
+    icon: faReply,
+    message: '返信', // アイコンの説明
+    ariaLabel: '返信', // アクセシビリティラベル
+  },
+  {
+    icon: faFlag,
+    message: 'マークをつける', // アイコンの説明
+    ariaLabel: 'マーク', // アクセシビリティラベル
+  },
+  {
+    icon: faTrash,
+    message: 'ゴミ箱に移動', // アイコンの説明
+    ariaLabel: 'ゴミ箱', // アクセシビリティラベル
+  },
+  {
+    icon: faEllipsisH,
+    message: 'その他の操作', // アイコンの説明
+    ariaLabel: 'その他', // アクセシビリティラベル
+  },
 ];
 
 // 頻繁に使用される共通のスタイルを定数として定義
 export const COMMON_STYLES = {
+  // Flexboxで中央に配置
   FLEX_CENTER: 'flex items-center justify-center',
+  // 角を丸くする
   ROUNDED: 'rounded',
+  // ホバー時の色変更にトランジションを適用
   TRANSITION: 'transition-colors duration-200',
 };
 
