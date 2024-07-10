db704df76b93f01f8fefd1681384c4386e2de798
kuwa124 <c0mwgave1407@gmail.com>
2024-07-10 16:27:35 +0900

    💡 コメントを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,16 @@ import Toolbar from '@/app/home/components/Toolbar';
 
 export default function Home() {
   return (
-    <div className='flex flex-col h-screen bg-gray-100'>
+    <div
+      // フレックスコンテナ、縦方向に配置、画面全体の高さ、背景色
+      className='flex flex-col h-screen bg-gray-100'
+    >
       <Header />
       <Toolbar /> {/* メインコンテンツ */}
-      <div className='flex flex-1 overflow-hidden'>
+      <div
+        // フレックスコンテナ、メインコンテンツの高さを埋める、オーバーフロー隠す
+        className='flex flex-1 overflow-hidden'
+      >
         {/* サイドバー */}
         <Sidebar />
         <MailBox />
